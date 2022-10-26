810a6bc94b963056c58a852eb5e4d87292c98115
danyel-dev <danyelpinheiro154@hotmail.com>
2022-10-26 13:21:15 -0300

    models criados

---
+++ core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class List(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+
+    def __name__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    List = models.ForeignKey(List, on_delete=models.CASCADE)
+    done = models.BooleanField()
+
+
+    def __name__(self):
+        return self.name
