a965a073127db5823a2d735f7529c5d95f37225c
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 <danyel-dev>
2023-01-26 18:19:09 -0300

    activating the system of token

--- core/views.py
+++ core/views.py
@@ -1,17 +1,18 @@
 from .models import List, Item
 from .Serializers import ListSerializer, ItemSerializer
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import permissions, authentication
 
 
 class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
     permission_classes = [permissions.IsAuthenticated]
-
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
 
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
