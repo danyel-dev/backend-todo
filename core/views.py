4340598560639654f39b42c3e4a7409acc463574
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 <danyel-dev>
2023-01-26 17:43:45 -0300

    ViewSet of listing of items created

--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
-from .models import List
+from .models import List, Item
+from .Serializers import ListSerializer, ItemSerializer
 from rest_framework import viewsets
 from rest_framework import permissions
-from .Serializers import ListSerializer
 
 
 class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
     permission_classes = [permissions.IsAuthenticated]
--- setup/urls.py
+++ setup/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from .views import UserViewSet, GroupViewSet
-from core.views import ListViewSet
+from core.views import ListViewSet, ItemViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(r'lists', ListViewSet)
+router.register(r'items', ItemViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
